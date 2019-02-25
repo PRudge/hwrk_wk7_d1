6d42e261bd1009405be40177eb79def2f5b86d81
Pauline Rudge <paulinerudge@me.com>
2019-02-25 22:02:00 +0000

    added factorisation

--- hw_pubsub_prime_checker_start/src/views/result_view.js
+++ hw_pubsub_prime_checker_start/src/views/result_view.js
@@ -14,12 +14,15 @@ ResultView.prototype.bindEvents = function () {
 ResultView.prototype.displayResult = function (resultDets) {
    const resultElement = document.querySelector('#result');
    const largestPrime = document.querySelector("#largest-prime");
+   const theFactors = document.querySelector("#factors");
 
    const displayRes = this.isPrimeOrNotText(resultDets);
    const displayLargestPrime = this.theLargestPrime(resultDets);
+   const displayFactors = this.getTheFactors(resultDets);
 
    resultElement.textContent = displayRes;
    largestPrime.textContent = displayLargestPrime;
+   theFactors.textContent = displayFactors;
 };
 
 ResultView.prototype.isPrimeOrNotText = function (resultDets) {
@@ -38,6 +41,36 @@ ResultView.prototype.theLargestPrime = function (resultDets) {
     largestPrime = "";
   }
   return largestPrime;
+};
+
+ResultView.prototype.getTheFactors = function (resultDets) {
+  if (resultDets.result === false){
+  const num = resultDets.number;
+  const maxFactorNum = Math.floor(Math.sqrt(num));
+    const factorArr = [];
+    let count = 0;  //count of factors found < maxFactorNum.
+
+    for (let i = 1; i <= maxFactorNum; i++) {
+        //inserting new elements in the middle using splice
+        if (num % i === 0) {
+            factorArr.splice(count, 0, i);
+            let otherFactor = num / i; //the other factor
+            if (i != otherFactor) {
+                //insert these factors in the front of the array
+                factorArr.splice(-count, 0, otherFactor);
+            }
+            count++;
+        }
+    }
+
+    //swapping first and last elements
+    let lastIndex = factorArr.length - 1;
+    let temp = factorArr[lastIndex];
+    factorArr[lastIndex] = factorArr[0];
+    factorArr[0] = temp;
+
+    return `Factors of ${resultDets.number}  are : ${factorArr}`;
+  }
 };
 
 module.exports = ResultView;
